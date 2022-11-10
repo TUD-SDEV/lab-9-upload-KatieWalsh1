50d8f7d6c1f4c7cf2f4280c3d1a6752f93dad423
Katie Walsh <x00182751@mytudublin.ie>
2022-11-10 11:33:44 +0000

    lab 9 part 2 commit

--- booksproject/settings.py
+++ booksproject/settings.py
@@ -40,9 +40,10 @@ INSTALLED_APPS = [
     #local
     'accounts',
     'pages',
+    'books',
     #3rd party
-    'crispy_forms',
-    'crispy_bootstrap5',
+    #'crispy_forms',
+    #'crispy_bootstrap5',
 ]
 
 MIDDLEWARE = [
